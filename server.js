1ade88c3633702113e636ba8d6367010dc34fa44
Antek <zse.poczta@gmail.com>
2019-12-04 00:57:48 +0100

    version 1.0.8

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
 app.use("/api/games", games);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
